f695766714c2be1b6cb2834ed9c95a7eff1396df
Caue Robattini <cauegr18@gmail.com>
2025-05-01 21:06:03 -0300

    Payment, one-to-one association

--- src/main/java/com/firstprojectspring/course/config/TestConfig.java
+++ src/main/java/com/firstprojectspring/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.firstprojectspring.course.entities.Category;
 import com.firstprojectspring.course.entities.Order;
 import com.firstprojectspring.course.entities.OrderItem;
+import com.firstprojectspring.course.entities.Payment;
 import com.firstprojectspring.course.entities.Product;
 import com.firstprojectspring.course.entities.User;
 import com.firstprojectspring.course.entities.enums.OrderStatus;
@@ -82,5 +83,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 	}
 }
